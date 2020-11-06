0d7cedeb3045121d55150be2b08c2488845480f0
vlad <vlad.riabtsev@live.ca>
2020-11-06 16:24:33 -0500

    wip

---
+++ vSharpStudio.common/IDbSchema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ViewModelBase;
+
+namespace vSharpStudio.common
+{
+    public partial interface IDbSchema
+    {
+        string DbSchemaStr { get; }
+        string KeyNameStr { get; }
+        string PKeyTypeStr { get; }
+        string IdGeneratorStr { get; }
+        string VersionFieldNameStr { get; }
+        string LP { get; }
+        string RP { get; }
+    }
+}
