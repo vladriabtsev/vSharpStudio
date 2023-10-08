aee90abecc4174999fff5a665274a208b4734ad7
3dots <3dot@live.ca>
2023-10-08 06:58:09 -0400

    Resize/position

--- vSharpStudio/MainWindow.xaml.cs
+++ vSharpStudio/MainWindow.xaml.cs
@@ -20,10 +20,19 @@ namespace vSharpStudio
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+
+        double _defaultWidth = 1000;
+        double _defaultHeight = 700;        
+
+        #endregion
+
+        #region Events
+
         public MainWindow()
         {
             InitializeComponent();
-            ResizeToScreenSize();
+            ResizeAndPosition();
 
             if (System.ComponentModel.DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
             {
@@ -33,11 +42,40 @@ namespace vSharpStudio
             // vSharpStudio.std.ApplicationLogging.LoggerFactory.AddProvider(new Serilog.Sinks.File.PeriodicFlushToDiskSink()
         }
 
-        void ResizeToScreenSize()
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Helpers
+
+        void ResizeAndPosition()
         {
             WpfScreen s = WpfScreen.GetScreenFrom(this);
 
+            double margin = 20;
+            double maxHeight = s.WorkingArea.Height - 2 * margin;
+            double maxWidth = s.WorkingArea.Width - 2 * margin;
 
+            if (maxHeight < _defaultHeight) Height = maxHeight;
+            else Height = _defaultHeight;
+
+            if (maxWidth < _defaultWidth) Width = maxWidth;
+            else Width = _defaultWidth;
+
+            double magicXNumber = 120.0 / 1920.0;
+            Left = magicXNumber * s.WorkingArea.Width;
+
+            Top = s.WorkingArea.Height / 2 - Height / 2;
         }
+
+        #endregion        
     }
 }
